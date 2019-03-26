166c1bb3ce892a9f0d53ecf7c764e42ed29392e2
nicolas <nikochapon@hotmail.com>
2019-03-26 21:58:57 +0100

    css bien attaque sur tour dates

--- Front/src/components/Tour/Date.js
+++ Front/src/components/Tour/Date.js
@@ -3,6 +3,8 @@
  */
 import React from 'react';
 import Proptypes from 'prop-types';
+import { MdStar, MdPinDrop } from 'react-icons/md';
+import { FaTicketAlt } from 'react-icons/fa';
 
 
 /**
@@ -14,18 +16,41 @@ import Proptypes from 'prop-types';
  * Code
  */
 
-const Date = ({ date, city, venue }) => (
-  <div className="tour-date">
-    <div className="tour-date-item">{date}</div>
-    <div className="tour-date-item">{city}</div>
-    <div className="tour-date-item">{venue}</div>
-  </div>
-);
+const Date = ({
+  date,
+  city,
+  venue,
+  tickets,
+}) => {
+  const splitDate = date.split('/');
+
+  return (
+    <div className="tour-date">
+      <div className="tour-date-date">
+        <p>
+          {splitDate[0]}
+          <MdStar className="tour-date-date-star" />
+          {splitDate[1]}
+        </p>
+        <p className="tour-date-date-year">{splitDate[2]}</p>
+      </div>
+      <div className="tour-date-city">{city}</div>
+      <div className="tour-date-venue">{venue}</div>
+      <a href="" target="_blank" className="tour-date-map">
+        <MdPinDrop />
+      </a>
+      <a href={tickets} className="tour-date-ticket">
+        <FaTicketAlt />
+      </a>
+    </div>
+  );
+};
 
 Date.propTypes = {
   date: Proptypes.string.isRequired,
   city: Proptypes.string.isRequired,
   venue: Proptypes.string.isRequired,
+  tickets: Proptypes.string.isRequired,
 };
 
 /**
