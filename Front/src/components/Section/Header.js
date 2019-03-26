1d798f7ef30afdb26c5bb6ac040a42d9848694aa
nicolas <nikochapon@hotmail.com>
2019-03-26 10:39:09 +0100

    dynamisation de la creation des sections avec utilisation de css in js pour les photos de background

--- Front/src/components/Section/Header.js
+++ Front/src/components/Section/Header.js
@@ -2,6 +2,8 @@
  * NPM import
  */
 import React from 'react';
+import styled from 'styled-components';
+import Proptypes from 'prop-types';
 
 /**
  * Local import
@@ -13,11 +15,26 @@ import React from 'react';
 /**
  * Code
  */
-const Header = () => (
-  <div id="header">
-    Je suis le header.
-  </div>
-);
+const Header = ({ name, title, bgp }) => {
+  const Heading = styled.header`
+    height: 60vh;
+    background-image: url(${bgp});
+    background-position: cover;
+  `;
+
+  return (
+    <Heading id={name} className="header">
+      <h1 className="header-title">{title}</h1>
+    </Heading>
+  );
+};
+
+// Props validation
+Header.propTypes = {
+  name: Proptypes.string.isRequired,
+  title: Proptypes.string.isRequired,
+  bgp: Proptypes.string.isRequired,
+};
 
 /**
  * Export
