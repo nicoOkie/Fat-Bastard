7dc1b822745dd7f909532dff7fe86e8e989cc234
nicolas <nikochapon@hotmail.com>
2019-03-27 21:12:06 +0100

    tests sur les barres de separation des news concluants! mise en forme a continuer

--- Front/src/components/News/Article.js
+++ Front/src/components/News/Article.js
@@ -15,20 +15,24 @@ import React from 'react';
  * Code
  */
 const Article = () => (
-  <article className="news-article">
-    <header className="news-article-header">
-      <h2 className="news-article-header-title">Title</h2>
-      <p className="news-article-header-date">Date</p>
-    </header>
-    <section className="news-article-section">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Laborum magni molestias tempore officiis cumque id, repudiandae sint
-        perspiciatis ullam fugit, placeat porro dolorum exercitationem
-        voluptates? Nisi maiores sit facere doloremque?
-      </p>
-    </section>
-
-  </article>
+  <>
+    <div className="news-split" />
+    <article className="news-article">
+      <header className="news-article-header">
+        <h2 className="news-article-header-title">Lorem ipsum dolor sit amet</h2>
+      </header>
+      <aside className="news-article-aside">
+        <p className="news-article-aside-date">27|03 <br /> 2019</p>
+      </aside>
+      <section className="news-article-section">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Laborum magni molestias tempore officiis cumque id, repudiandae sint
+          perspiciatis ullam fugit, placeat porro dolorum exercitationem
+          voluptates? Nisi maiores sit facere doloremque?
+        </p>
+      </section>
+    </article>
+  </>
 );
 
 
