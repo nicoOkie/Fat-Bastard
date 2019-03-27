6560812de1e37206671cdf28e2f7f275e84c748c
nicolas <nikochapon@hotmail.com>
2019-03-27 15:27:09 +0100

    ajout de l'id aux sections depuis le composant Section/index

---
+++ Front/src/components/News/index.js
@@ -0,0 +1,28 @@
+/**
+ * NPM Import
+ */
+import React from 'react';
+
+
+/**
+ * Local Import
+ */
+
+// Styles
+import './news.scss';
+
+/**
+ * Code
+ */
+const News = () => (
+  <div className="news">
+    Des nouvelles fariches comme la rosée du  matin !
+  </div>
+);
+
+
+/**
+ * Export
+ */
+
+export default News;
