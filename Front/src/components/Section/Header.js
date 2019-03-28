68edc8f3895b197bb7b6041530183ac1096387cb
nicolas <nikochapon@hotmail.com>
2019-03-28 10:01:31 +0100

    modif style header

--- Front/src/components/Section/Header.js
+++ Front/src/components/Section/Header.js
@@ -20,9 +20,6 @@ const Header = ({ name, title, bgp }) => (
     <ParallaxProvider>
       <ParallaxBanner
         id={name}
-        style={{
-          height: '74vh',
-        }}
         layers={[
           {
             image: `${bgp}`,
