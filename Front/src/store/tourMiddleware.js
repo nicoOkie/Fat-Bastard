9651a4f537ececbb13cda253d430c796c7e17d65
nicolas <nikochapon@hotmail.com>
2019-04-01 22:16:50 +0200

    slight changes in the middlewares tour and section -> using the apiURL instead of the local url

--- Front/src/store/tourMiddleware.js
+++ Front/src/store/tourMiddleware.js
@@ -5,14 +5,14 @@ import axios from 'axios';
 // Import
 import { LOAD_TOUR_DATES, receivedTourDates } from 'src/store/reducer';
 
-// const apiURL = 'http://92.243.8.90/fat/back/wp-json';
+const apiURL = 'http://92.243.8.90/fat/back/wp-json';
 
 
 const tourMiddleware = store => next => (action) => {
   switch (action.type) {
     case LOAD_TOUR_DATES:
       axios
-        .get('http://localhost/fat-apotheose/back/wp-json/wp/v2/tourdates')
+        .get(`${apiURL}/wp/v2/tourdates`)
         .then(({ data }) => {
           store.dispatch(receivedTourDates(data));
         })
