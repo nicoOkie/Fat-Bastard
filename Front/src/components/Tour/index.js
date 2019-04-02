fe25caf8585ec7122a57869e2e7cb56946e75dd6
nicolas <nikochapon@hotmail.com>
2019-04-02 13:52:44 +0200

    tourdates sorted in the right way!

--- Front/src/components/Tour/index.js
+++ Front/src/components/Tour/index.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { orderBy } from 'lodash';
+import { sortBy } from 'lodash';
 
 
 /**
@@ -20,30 +20,37 @@ import './tour.scss';
 const Tour = ({ dates, loaded }) => {
 
   if (loaded) {
-    console.log(dates);
-    const newDates = dates.map(date => (
+    let newDates = dates.map(date => (
       {
         id: date.id,
-        date: date.custom_fields.date,
+        date: date.custom_fields.date[0],
+        city: date.custom_fields.city[0],
+        venue: date.custom_fields.venue[0],
+        map: date.custom_fields.map[0],
+        tickets: date.custom_fields.tickets[0],
       }
     ));
+
+    newDates = sortBy(newDates, ['date']);
     console.log(newDates);
+
+    return (
+      <div className="tour special-width">
+        {newDates.map(date => (
+          <Date key={date.id} {...date} />
+        ))}
+      </div>
+    );
   }
 
-
-  return (
-    <div className="tour special-width">
-      {/* {dates.map(date => (
-        <Date key={date.id} {...date.custom_fields} />
-      ))} */}
-    </div>
-  );
+  return (<p>loading</p>);
 };
 
 Tour.propTypes = {
   dates: PropTypes.arrayOf(
     PropTypes.object.isRequired,
   ).isRequired,
+  loaded: PropTypes.bool.isRequired,
 };
 /**
  * Export
