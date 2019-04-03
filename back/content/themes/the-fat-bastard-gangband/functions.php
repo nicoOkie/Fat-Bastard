832b04ae378db3a9c6d685f1621d473dc47e672e
AmelieCrn <cornu.amelie91@gmail.com>
2019-04-03 10:15:19 +0200

    file custom-tables

--- back/content/themes/the-fat-bastard-gangband/functions.php
+++ back/content/themes/the-fat-bastard-gangband/functions.php
@@ -6,32 +6,4 @@ require get_theme_file_path('inc/menus.php');
 
 require get_theme_file_path('inc/customizer.php');
 
-global $wpdb;
-
-$charset_collate = $wpdb->get_charset_collate();
-
-$newsletter_table_name = $wpdb->prefix . 'newsletter';
-
-$newsletter_sql = "CREATE TABLE IF NOT EXISTS $newsletter_table_name (
-	id int NOT NULL AUTO_INCREMENT,
-    firstname varchar(255) NOT NULL,
-	lastname varchar(255) NOT NULL,
-	email varchar(255) NOT NULL,
-	PRIMARY KEY  (id)
-) $charset_collate;";
-
-$alert_email_table_name = $wpdb->prefix . 'alert_email';
-
-$alert_sql = "CREATE TABLE IF NOT EXISTS $alert_email_table_name (
-	id int NOT NULL AUTO_INCREMENT,
-    firstname varchar(255) NOT NULL,
-	lastname varchar(255) NOT NULL,
-	email varchar(255) NOT NULL,
-	department int NOT NULL,
-	PRIMARY KEY  (id)
-) $charset_collate;";
-
-require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
-dbDelta($newsletter_sql);
-dbDelta($alert_sql);
+require get_theme_file_path('inc/custom-tables.php');
