c6a408803f831efb7cb77de06693ee741a7a1125
Karine Bochet <k.bochet@orange.fr>
2019-04-08 17:52:00 +0200

    custom-login + ajout require dans index.php

--- back/content/themes/the-fat-bastard-gangband/functions.php
+++ back/content/themes/the-fat-bastard-gangband/functions.php
@@ -2,6 +2,8 @@
 
 require get_theme_file_path('inc/setup.php');
 
+require get_theme_file_path('inc/enqueue-scripts.php');
+
 require get_theme_file_path('inc/menus.php');
 
 require get_theme_file_path('inc/customizer.php');
@@ -9,4 +11,5 @@ require get_theme_file_path('inc/customizer.php');
 require get_theme_file_path('inc/custom-endpoint.php');
 
 require get_theme_file_path('inc/widgets.php');
+
 require get_theme_file_path('inc/custom-tables.php');
